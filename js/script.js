ec0a3775fcfcc003c778ffa16abc7c1ed970457c
Laprevote Emma <emma.laprevote@laplateforme.io>
2021-04-08 09:01:42 +0200

    responsive page enter part1

--- js/script.js
+++ js/script.js
@@ -5,10 +5,11 @@ function changeImg() {
     setTimeout("changeImg()", 8000)
 }
 
-window.onload = function(){
+/*window.onload = function(){
     changeImg();
-}
+}*/
 
+// SCRIPT PAGE TRANSITION BARBA / GSAP
 const wipe = document.querySelector('.wipe-transition');
 
 let TLAnim = new TimelineMax();
@@ -50,6 +51,46 @@ barba.init({
     ]
 
 })
+
+// SCRIPT JQUERIES RESPONSIVE
+
+
+    if(window.matchMedia("(min-width:280px)").matches && window.matchMedia("(max-width:1024px)").matches){
+
+        $('#welcomeHome').css("flexDirection", "column");
+        $('#containerImg').css("height", "30em");
+
+        $('#logoEmka').css("borderRight", "none");
+        $('#logoEmka').css("width", "80%");
+        $('#logoEmka').css({
+            margin: "0 auto",
+            marginTop: "-10em",
+        });
+
+        $('#portraitEmka').css("marginLeft", "25%");
+
+        $('#changeImg').removeClass().addClass("imgChangeResponse");
+        $('#typoLogoEmka').css("width", "55%");
+        $('#typoLogoEmka').css({
+            margin: "0 auto"
+        });
+
+        $('#upImg').empty();
+        $('#upImg').append(buttonEnter ());
+
+        $('#upImg').removeClass().addClass("upImageResponse");
+        $('#imgEnter').remove().addClass("imgEnterResponse");
+
+
+
+    }
+
+    function buttonEnter () {
+        let html = "";
+        html += "<a id=\"enterSite\" href=\"./accueil.php\"><img id=\"imgEnter\" src=\"../images/bulleEnterResponsive.png\"></a>";
+
+        return html;
+    }
 
 
 
