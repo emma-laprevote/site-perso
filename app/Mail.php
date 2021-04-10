e2d1f9d72cf6b9f8bbb528707e6e3bbfc39c3ab9
Laprevote Emma <emma.laprevote@laplateforme.io>
2021-04-10 16:50:30 +0200

    phpMailer final

--- app/Mail.php
+++ app/Mail.php
@@ -25,14 +25,13 @@ require '../vendor/phpmailer/phpmailer/src/SMTP.php';
             $mail = new PHPMailer(true);
 
             //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-            $mail->SMTPDebug = 2;
-            $mail->Mailer = "smtp";
-            $mail->Host = "localhost";
-            $mail->Port = 587;
-            $mail->SMTPAuth = true; // turn on SMTP authentication
-            $mail->Username = "emma.laprevote@laplateforme.io"; // SMTP username
-            $mail->Password = "4DacYAZ2"; // SMTP password
-            $Mail->Priority = 1;                                    //Send using SMTP
+            $mail->isSMTP();                                            //Send using SMTP
+            $mail->Host = 'smtp.gmail.com';                     //Set the SMTP server to send through
+            $mail->SMTPAuth = true;                                   //Enable SMTP authentication
+            $mail->Username = 'laprevote.emma@gmail.com';                     //SMTP username
+            $mail->Password = 'lkanizmbebajdkmn';                               //SMTP password
+            $mail->SMTPSecure = 'tls';         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+            $mail->Port = 587;                                  //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
             //Recipients
             $mail->setFrom($email);
